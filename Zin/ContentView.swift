c2a41745480bf0703ab16b81ee6827f6333bc01c
zenith404z <zainab.hawsa98@gmail.com>
2023-10-05 12:30:06 +0300

    Initial Commit

---
+++ Zin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zin
+//
+//  Created by zainab hawsa on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zin/ZinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZinApp.swift
+//  Zin
+//
+//  Created by zainab hawsa on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ZinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
